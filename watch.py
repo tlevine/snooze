3be33f6d4ddc0a5c61892c2841a2c2e2c1a0af88
Thomas Levine <thomas.levine@gmail.com>
2012-05-22 09:59:22 -0400

    fancy music playing

--- watch.py
+++ watch.py
@@ -15,6 +15,8 @@ def loadtime(text):
     return datetime.datetime.strptime(text, timeformat)
 
 while True:
+    print chr(27) + "[2J" #Clear terminal
+    print datetime.datetime.now().strftime(timeformat)
     status = json.loads(urlopen('http://alarm.thomaslevine.com/alarm').read())
     last_alert = loadtime(status['last_alert']) + datetime.timedelta(seconds = framerate)
     current_time = loadtime(status['current_time'])
@@ -22,7 +24,9 @@ while True:
         raise Exception('status')
     elif last_alert > current_time:
         print('<blinking lights>')
-        os.system('mplayer ~/Music/DragonForce/*/*')
+        os.system('mplayer ~/Music/DragonForce/*/* &>/dev/null &')
+        raw_input('You are being summoned! Press any key to stop the music')
+        os.system('killall mplayer')
     else:
         print('No alarm')
         sleep(framerate)
